16eb1b4ec6bc1deed6d9cd1ec2293612f1747819
Ganezar <ganez2016@gmail.com>
2024-06-20 18:30:45 +0700

    collectable stone
    berhasil, setiap collect stone akan menambah ke ui nya

--- Assets/script/pickupstone.cs
+++ Assets/script/pickupstone.cs
@@ -3,76 +3,88 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class pickupstone : MonoBehaviour
+public class pickupLetter : MonoBehaviour
 {
     public GameObject collectTextObj, intText;
     public AudioSource pickupSound, ambianceLayer1, ambianceLayer2, ambianceLayer3, ambianceLayer4, ambianceLayer5, ambianceLayer6, ambianceLayer7, ambianceLayer8;
     public bool interactable;
-    public static int pagesCollected;
+    public static int stonescollected;
     public Text collectText;
 
     void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("MainCamera"))
         {
+            Debug.Log("MainCamera detected in trigger.");
             intText.SetActive(true);
             interactable = true;
         }
     }
+
     void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("MainCamera"))
         {
+            Debug.Log("MainCamera exited trigger.");
             intText.SetActive(false);
             interactable = false;
         }
     }
+
     void Update()
     {
-        if(interactable == true)
+        if (interactable)
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
-                pagesCollected = pagesCollected + 1;
-                collectText.text = pagesCollected + "/8 pages";
+                Debug.Log("E key pressed while interactable.");
+
+                stonescollected += 1;
+                collectText.text = stonescollected + "/8 stones";
                 collectTextObj.SetActive(true);
                 pickupSound.Play();
-                if(pagesCollected == 1)
-                {
-                    ambianceLayer1.Play();
-                }
-                if (pagesCollected == 2)
-                {
-                    ambianceLayer2.Play();
-                }
-                if (pagesCollected == 3)
-                {
-                    ambianceLayer3.Play();
-                }
-                if (pagesCollected == 4)
-                {
-                    ambianceLayer4.Play();
-                }
-                if (pagesCollected == 5)
-                {
-                    ambianceLayer5.Play();
-                }
-                if (pagesCollected == 6)
-                {
-                    ambianceLayer6.Play();
-                }
-                if (pagesCollected == 7)
-                {
-                    ambianceLayer7.Play();
-                }
-                if (pagesCollected == 8)
-                {
-                    ambianceLayer8.Play();
-                }
+                PlayAmbianceLayer(stonescollected);
+
                 intText.SetActive(false);
                 this.gameObject.SetActive(false);
                 interactable = false;
+
+                Debug.Log("Item picked up. stones collected: " + stonescollected);
             }
         }
+    }
+
+    void PlayAmbianceLayer(int layer)
+    {
+        switch (layer)
+        {
+            case 1:
+                ambianceLayer1.Play();
+                break;
+            case 2:
+                ambianceLayer2.Play();
+                break;
+            case 3:
+                ambianceLayer3.Play();
+                break;
+            case 4:
+                ambianceLayer4.Play();
+                break;
+            case 5:
+                ambianceLayer5.Play();
+                break;
+            case 6:
+                ambianceLayer6.Play();
+                break;
+            case 7:
+                ambianceLayer7.Play();
+                break;
+            case 8:
+                ambianceLayer8.Play();
+                break;
+        }
+        intText.SetActive(false);
+        this.gameObject.SetActive(false);
+        interactable = false;
     }
 }
