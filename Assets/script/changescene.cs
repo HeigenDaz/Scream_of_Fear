1584444234c6d0982573654ed2d631deb8b97b0c
Ganezar <ganez2016@gmail.com>
2024-06-21 18:10:25 +0700

    fix gameplay
    update monster and system gameplay

--- Assets/script/changescene.cs
+++ Assets/script/changescene.cs
@@ -6,11 +6,19 @@ using UnityEngine.SceneManagement;
 public class changeSceneTrigger : MonoBehaviour
 {
     public string sceneName;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene(sceneName);
+            if (pickupLetter.stonescollected >= 8) // Periksa apakah 8 batu sudah terkumpul
+            {
+                SceneManager.LoadScene(sceneName);
+            }
+            else
+            {
+                Debug.Log("Collect all 8 stones to finish the game!");
+            }
         }
     }
 }
